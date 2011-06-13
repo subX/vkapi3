c689fe5ab0cd46fb5eaeb283380f1c38e661d08e
mr.subX <User@.(none)>
2011-06-13 15:01:06 +0300

    First point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vkapi3::Application.config.secret_token = '35bb8147b3fc8fe55e8ddc8ce8130ea3ea50aaa76dabab7cae068fc45f557018d8a57de1c0e3ce810f1671749c988b83b6599c0ab37c449c71af6bc4f81445b5'
